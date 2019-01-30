df3ab49ca1d0a47c5df41c5858fd537ecd1cf719
SAURABH <saurabh@SAURABHs-MacBook-Air.local>
2019-01-30 23:10:14 +0530

    Updates

--- precache-manifest.e2e908c96eca826e141cceecb0114aac.js
+++ precache-manifest.e2e908c96eca826e141cceecb0114aac.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/to-do-app-react/static/js/runtime~main.602a19bf.js"
   },
   {
-    "revision": "3947990c58dde4a7d766",
-    "url": "/to-do-app-react/static/js/main.3947990c.chunk.js"
+    "revision": "67131891b99133ecd8b2",
+    "url": "/to-do-app-react/static/js/main.67131891.chunk.js"
   },
   {
     "revision": "eed27d798e481bf85c35",
     "url": "/to-do-app-react/static/js/1.eed27d79.chunk.js"
   },
   {
-    "revision": "414f0bee49108cbac1baa1760117b0ae",
+    "revision": "ca6b70fbafd59be3478d9586498e7483",
     "url": "/to-do-app-react/index.html"
   }
 ];
